8c9833c20159ef2d2925460e8d9406ca6d50e421
Steven Yirenkyi <yirenkyisteven7@gmail.com>
2022-08-07 12:11:26 +0000

    get hashtags and urls

--- collect_tweets.py
+++ collect_tweets.py
@@ -1,9 +1,13 @@
+from html import entities
 from dotenv import load_dotenv
 from os import environ
 from tweepy import Client, Paginator, Tweet, Response
 from datetime import datetime
 from bs4 import BeautifulSoup
+from typing import Any, TypedDict
+
 import requests
+
 
 load_dotenv()
 
@@ -15,8 +19,8 @@ def search_tweets(client: Client) -> list[Tweet]:
     end_time = datetime.strptime("2022-08-06", date_pattern)
     tweets: list[Tweet] = []
 
-    for response in Paginator(client.search_all_tweets, query, start_time=start_time, end_time=end_time, max_results=100,
-                              tweet_fields=["author_id", "public_metrics", "entities"], limit=3):
+    for response in Paginator(client.search_all_tweets, query, start_time=start_time, end_time=end_time, max_results=10,
+                              tweet_fields=["author_id", "public_metrics", "entities"], limit=1):
         response: Response
 
         tweets.extend(response.data)
@@ -24,7 +28,7 @@ def search_tweets(client: Client) -> list[Tweet]:
     return tweets
 
 
-def get_webpage_meta(url: str) -> dict:
+def get_webpage_meta(url: str, tweet_id: str) -> dict:
     try:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"
@@ -32,13 +36,77 @@ def get_webpage_meta(url: str) -> dict:
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, "html.parser")
 
+        og_title = soup.find("meta", property="og:title")
+        og_description = soup.find("meta", property="og:description")
+        title = soup.title
+        description = soup.find("meta", attrs={"name": "description"})
+
         return {
-            "title": soup.title.string,
-            "description": soup.find("meta", attrs={"name": "description"})["content"]
+            "title": og_title["content"] if og_title else title.string if title else "",
+            "description": og_description["content"] if og_description else description["content"] if description else ""
         }
-    except:
+
+    except requests.exceptions.ConnectionError:
+        # TODO: LOG ERRORS
+        return None
+    except Exception as e:
+        print(e)
         return None
 
+
+def get_hashtags(tweet: Tweet) -> dict:
+    entities = tweet.entities
+    if entities is None:
+        return dict(hashtag_count=0, hashtags="")
+
+    hashtag_list = entities.get("hashtags")
+    if hashtag_list is None:
+        return dict(hashtag_count=0, hashtags="")
+
+    hashtags = []
+    for hashtag_dict in hashtag_list:
+        hashtags.append(hashtag_dict["tag"])
+
+    return dict(hashtag_count=len(hashtags), hashtags=" ".join(hashtags))
+
+
+def get_urls(tweet: Tweet) -> dict[str, Any]:
+    error_dict = dict(webpage_description="",
+                      webpage_title="", url_count=0, url="")
+    entities = tweet.entities
+    if entities is None:
+        return error_dict
+
+    url_list = entities.get("urls")
+    if url_list is None:
+        return error_dict
+
+    descriptions = []
+    titles = []
+    urls = []
+    for url_dict in url_list:
+        url = url_dict.get("unwound_url") or url_dict.get("expanded_url")
+
+        if url.startswith("https://twitter.com/"):
+            descriptions.append("twitter_link")
+            titles.append("twitter_link")
+            urls.append(url)
+            continue
+
+        meta = get_webpage_meta(url, tweet.id)
+        if meta is None:
+            continue
+        descriptions.append(meta.get("description"))
+        titles.append(meta.get("title"))
+        urls.append(url)
+
+    delimiter = " ♣♣ "
+    return{
+        "webpage_description": delimiter.join(descriptions),
+        "webpage_title": delimiter.join(titles),
+        "url_count": len(urls),
+        "url": delimiter.join(urls)
+    }
 
 
 
@@ -46,13 +114,17 @@ if __name__ == "__main__":
     client = Client(
         bearer_token=environ["ACADEMIC_BEARER_TOKEN"], wait_on_rate_limit=True)
 
-    # tweets = search_tweets(client)
+    tweets = search_tweets(client)
 
-    # for tweet in tweets:
-    #     print(tweet.id)
-    #     print(tweet.text)
-    #     print("\n..........................................")
+    for tweet in tweets:
+        # print(tweet.id)
+        # print(tweet.text)
+        # print(tweet.author_id)
+        # print(tweet.public_metrics)
+        # print(tweet.entities)
+        e = get_hashtags(tweet)
+        print(e)
+        f = get_urls(tweet)
+        print(f)
 
-    url = "https://realpython.com/python-web-scraping-practical-introduction/"
-    df = get_webpage_description(url)
-    print(df)
+        print("\n..........................................")
