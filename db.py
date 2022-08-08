641844f56bf8250c391dc766a7cb046b8ea74c3f
Steven Yirenkyi <yirenkyisteven7@gmail.com>
2022-08-08 16:18:54 +0000

    save data to cloud db

--- db.py
+++ db.py
@@ -1,16 +1,20 @@
+from dotenv import load_dotenv
 from pymongo import MongoClient
 from pymongo.collection import Collection
+from os import environ
+
+load_dotenv()
 
 
 try:
-    mongo_client = MongoClient()
+    mongo_url = environ["MONGODB_URL"]
+    mongo_client = MongoClient(mongo_url) if mongo_url else MongoClient()
     db = mongo_client["ml-disinformation"]
-except:
+except Exception as e:
+    print(e)
     print("Could not connect to database")
     exit()
 
 
 def get_collection(collection_name: str) -> Collection:
     return db[collection_name]
-
-
